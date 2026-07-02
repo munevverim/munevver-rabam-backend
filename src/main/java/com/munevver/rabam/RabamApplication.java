55de392700674166bb1ff92d5ccd1d7510a98ac5
munev <munevverv@mobiliz.com.tr>
2026-07-02 14:42:22 +0300

    proje  oluşturuldu

---
+++ src/main/java/com/munevver/rabam/RabamApplication.java
@@ -0,0 +1,13 @@
+package com.munevver.rabam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabamApplication.class, args);
+	}
+
+}
